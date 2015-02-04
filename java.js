b3477f08c11b86e7d63d7d28e793f149c898cafa
João Fernando Rosa Silva <jhone_fer@hotmail.com>
2015-02-04 16:35:22 -0200

    1 manipulação de dados
    Mudei o css do meu CV

---
+++ java.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+<title>Jhones </title>
+</head>
+  <body>
+    <script>
+      console.log(15 > 4);
+      console.log("Xiao Hui".length !==  122);
+      console.log("Goody Donaldson".length>=  8);
+      console.log(8*2=== 16);
+    </script>
+  </body>
+</html>
